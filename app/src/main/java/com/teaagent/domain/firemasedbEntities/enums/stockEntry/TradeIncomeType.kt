60aa3c58adf233b981f47b03ce2331ef0686c4d4
gunasindhus <gunasindhu.saikia@airfi.aero>
2022-11-17 21:40:10 +0530

    added    PORTFOLIO_2

--- app/src/main/java/com/teaagent/domain/firemasedbEntities/enums/stockEntry/TradeIncomeType.kt
+++ app/src/main/java/com/teaagent/domain/firemasedbEntities/enums/stockEntry/TradeIncomeType.kt
@@ -1,4 +1,4 @@
 package com.teaagent.domain.firemasedbEntities.enums.stockEntry
 enum class TradeIncomeType {
-        SELECT, MONTHLY, WEEKLY, DAILY, HOURLY,PORTFOLIO
+        SELECT, MONTHLY, WEEKLY, DAILY, HOURLY,PORTFOLIO_1,PORTFOLIO_2
 }
